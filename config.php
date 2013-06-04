d0ffe896b50b8db8c778a69b42b5044d6ef88320
Atul <amkcat9@gmail.com>
2013-06-04 17:19:53 +0530

    shoppingcart app config file

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$dbhost = "127.0.0.1";
+$dbuser = "atul";
+$dbpassword = "mypasswd";
+$dbdatabase = "shoppingcart";
+
+$config_basedir = "http://127.0.0.1/sites/shoppingcart/";
+
+$config_sitename = "Perfect Price";
+
+?>
+
