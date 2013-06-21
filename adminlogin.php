d8a7e7daa093dfce84f13301ea7863b89fde5951
Atul <amkcat9@gmail.com>
2013-06-21 18:29:08 +0530

    Admin login

---
+++ adminlogin.php
@@ -0,0 +1,53 @@
+<?php
+require("header.php");
+if(isset($_SESSION['SESS_ADMINLOGGEDIN']) == TRUE) {
+	header("Location: " . $config_basedir);
+}
+
+if(isset($_POST['submit'])) {
+	$loginsql = "SELECT * FROM admins WHERE username = '" . $_POST['userBox'] . "' AND password = '" . $_POST['passBox'] . "'";
+	$loginres = mysql_query($loginsql);
+	$numrows = mysql_num_rows($loginres);
+	
+	if($numrows == 1) {
+		$loginrow = mysql_fetch_assoc($loginres);
+		
+		$_SESSION['SESS_ADMINLOGGEDIN'] = 1;
+		
+		header("Location: " . $config_basedir . "adminorders.php");
+	}
+	else {
+		header("Location: " . $config_basedir . "adminlogin.php?error=1");
+	}
+}
+else {
+	echo "<h1>Admin Login</h1>";
+	
+	if($_GET['error'] == 1) {
+		echo "<strong>Incorrect username/password!</strong>";
+	}
+?>
+	<p>
+	<form action="<?php echo $_SERVER['SCRIPT_NAME']; ?>" method="POST">
+	<table>
+		<tr>
+			<td>Username</td>
+			<td><input type="textbox" name="userBox">
+		</tr>
+		<tr>
+			<td>Password</td>
+			<td><input type="password" name="passBox">
+		</tr>
+		<tr>
+			<td></td>
+			<td><input type="submit" name="submit" value="Log in">
+		</tr>
+	</table>
+	</form>
+
+<?php
+}
+
+require("footer.php");
+?>
+
